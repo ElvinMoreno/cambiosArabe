5f657bb4f4c9772543fa6f5ee2e898dff88fdd9e
MaicolRuiz01 <maicolandreyruga@ufps.edu.co>
2024-07-06 14:41:24 -0500

    formulario

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,6 +4,9 @@ import { NavbarPolaniaComponent } from './shared/navbar-polania/navbar-polania.c
 import { SidebarPolaniaComponent } from './shared/sidebar-polania/sidebar-polania.component';
 import { ContentPolaniaComponent } from './shared/content-polania/content-polania.component';
 import { MatSidenavModule } from '@angular/material/sidenav';
+import { FormControl, FormsModule, ReactiveFormsModule, Validator } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
 
 @Component({
   selector: 'app-root',
@@ -12,11 +15,12 @@ import { MatSidenavModule } from '@angular/material/sidenav';
     ContentPolaniaComponent,
     NavbarPolaniaComponent,
     SidebarPolaniaComponent,
-    MatSidenavModule
+    MatSidenavModule, FormsModule, ReactiveFormsModule,MatInputModule,MatFormFieldModule
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'entrada-Bolivares';
+ ;
 }
--- src/app/navbar/bancolombia/bancolombia.component.ts
+++ src/app/navbar/bancolombia/bancolombia.component.ts
@@ -1,12 +1,56 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSelectModule } from '@angular/material/select';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core';
+import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
+
 
 @Component({
   selector: 'app-bancolombia',
   standalone: true,
-  imports: [],
+  imports: [CommonModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    MatSelectModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    ReactiveFormsModule],
   templateUrl: './bancolombia.component.html',
   styleUrl: './bancolombia.component.css'
 })
-export class BancolombiaComponent {
+export class BancolombiaComponent {form: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+    this.form = this.fb.group({
+      nombreCuenta: ['', Validators.required],
+      fecha: ['', Validators.required],
+      cuentaBs: ['', Validators.required],
+      tipoPago: ['', Validators.required],
+      conversionAutomatica: ['', Validators.required],
+      comision: ['', Validators.required],
+      celular: ['', Validators.required],
+      cliente: ['', Validators.required],
+      cuentaPesos: ['', Validators.required],
+      cantidadBolivares: ['', Validators.required],
+      tasa: ['', Validators.required],
+      cedula: ['', Validators.required]
+    });
+  }
+
+  confirmarVenta() {
+    if (this.form.valid) {
+      console.log(this.form.value);
+    }
+  }
+
+  cancelar() {
+    this.form.reset();
+  }
 
 }
