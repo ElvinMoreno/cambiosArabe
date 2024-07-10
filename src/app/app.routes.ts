44e6891c97d90e96b19372abc4dd259821d0ad9b
MaicolRuiz01 <maicolandreyruga@ufps.edu.co>
2024-07-10 14:18:41 -0500

    se creo el consumo e la api del back y adeamas se realizo en login

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,8 +2,14 @@ import { Routes } from '@angular/router';
 import { BolivaresComponent } from './navbar/bolivares/bolivares.component';
 import { BancolombiaComponent } from './navbar/bancolombia/bancolombia.component';
 import { CajaComponent } from './navbar/caja/caja.component';
+import { LoginComponent } from './pages/login/login.component';
+import { RegistroComponent } from './pages/registro/registro.component';
+import { InicioComponent } from './pages/inicio/inicio.component';
 
 export const routes: Routes = [
+  {path:"",component:LoginComponent},
+  {path:"registro",component:RegistroComponent},
+  {path:"inicio",component:InicioComponent},
   {path: 'bolivares', component:BolivaresComponent},
   {path: 'bancolombia', component:BancolombiaComponent},
   {path: 'cajas', component:CajaComponent},
---
+++ src/app/services/acceso.service.ts
@@ -0,0 +1,26 @@
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { appsetting } from '../settings/appsetting';
+import { Usuario } from '../interfaces/Usuario';
+import { Observable } from 'rxjs';
+import { ResponseAcceso } from '../interfaces/ResponseAcceso';
+import { Login } from '../interfaces/login';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AccesoService {
+
+  private http=inject(HttpClient);
+  private baseUrl:string=appsetting.apiUrl;
+
+  constructor() { }
+
+  registrarse(objeto:Usuario):Observable <ResponseAcceso>{
+    return this.http.post<ResponseAcceso>(`${this.baseUrl}auth/register`,objeto)
+  }
+
+  login(objeto:Login):Observable <ResponseAcceso>{
+    return this.http.post<ResponseAcceso>(`${this.baseUrl}auth/login`,objeto)
+  }
+}
