71af7de470768934e9be3796bbc3fcdf58add486
MaicolRuiz01 <106454008+MaicolRuiz01@users.noreply.github.com>
2025-01-07 16:10:18 -0500

    nueva api key gemini

--- src/app/services/gemini.service.ts
+++ src/app/services/gemini.service.ts
@@ -8,7 +8,7 @@ import { Observable } from 'rxjs';
 export class GeminiService {
   private apiUrl =
     'https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash-latest:generateContent';
-  private apiKey = 'AIzaSyA1J5z8RLNgd0pYG7MvLE_2Lh3-eNSDRK0'; // Reemplaza con tu API Key
+  private apiKey = 'AIzaSyBQ3XaBynUwkLIM0fh325FAHj3CEdi6Lfc'; // Reemplaza con tu API Key
 
   constructor(private http: HttpClient) {}
 
